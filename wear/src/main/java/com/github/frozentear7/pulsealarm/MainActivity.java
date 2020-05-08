7f94e3fa5fff7dbb8ca33b18ea80247454d03ca1
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-05-08 19:15:18 +0200

    Add an example heart rate sensor handler

--- wear/src/main/java/com/github/frozentear7/pulsealarm/MainActivity.java
+++ wear/src/main/java/com/github/frozentear7/pulsealarm/MainActivity.java
@@ -1,21 +1,56 @@
 package com.github.frozentear7.pulsealarm;
 
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements SensorEventListener {
 
-    private TextView mTextView;
+    private static final String TAG = "MainActivity";
+    private TextView heartRateTextView;
+    SensorManager mSensorManager;
+    Sensor mHeartRateSensor;
+    SensorEventListener sensorEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        heartRateTextView = findViewById(R.id.heartRateTextView);
+        heartRateTextView.setText("Test");
 
-        // Enables Always-on
-        setAmbientEnabled();
+        mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
+        assert mSensorManager != null;
+        mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.i(TAG, "LISTENER REGISTERED.");
+
+        mSensorManager.registerListener(sensorEventListener, mHeartRateSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    public void onResume() {
+        super.onResume();
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+            String msg = "" + (int) event.values[0];
+            heartRateTextView.setText(msg);
+            Log.d(TAG, msg);
+            System.out.println(msg);
+        } else {
+            Log.d(TAG, "Unknown sensor type");
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "onAccuracyChanged - accuracy: " + accuracy);
     }
 }
